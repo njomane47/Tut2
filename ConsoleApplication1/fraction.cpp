c42b1304f3f988dd0df07252ff0af75f3deb0584
njomane47 <213518409@stu.ukzn.ac.za>
2015-03-10 13:29:43 +0200

    this is c++ file and header file

---
+++ ConsoleApplication1/fraction.cpp
@@ -0,0 +1,29 @@
+#pragma once
+#pragma once
+class Fraction
+{
+
+private:
+	int numr, deno;
+public:
+	Fraction(int num = 0, int den = 1);
+	~Fraction();
+
+	int getnumerator();
+	void Setnumerator(int num)
+		int getdenominator();
+	void Setdenominator(int num);
+
+
+
+	Fraction add(Fraction add);
+	Fraction subtract(Fraction subtract);
+	Fraction multiply(Fraction multiply);
+	Fraction division(Fraction division);
+
+	Fraction operator+(Fraction add);
+	Fraction operator-(Fraction subtract);
+	Fraction operator*(Fraction multiply);
+	Fraction operator/(Fraction division);
+	void print();
+};
